b79a884e131a91112b35d3f163580f4e48b79c68
Maggie Moreno <maggie@tubularlabs.com>
2018-02-28 15:19:54 -0800

    add a navbar because we deserve a home  button

---
+++ src/navbar.js
@@ -0,0 +1,50 @@
+import React, { PropTypes, Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
+
+export default class MyNavbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand href="/">MCS Registration App</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/new">New Student Form</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/returning">Returning Student Checkin</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/dance">Dance Form</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+};
