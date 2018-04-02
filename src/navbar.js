4345d3bebd9be0fd417a8fe11baaca336d76ee79
Maggie Moreno <margaret.m.moreno@gmail.com>
2018-04-01 22:41:39 -0700

    Revert "clean up nav"

--- src/navbar.js
+++ src/navbar.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
-
 import {
   Collapse,
-  ButtonDropdown,
-  UncontrolledDropdown,
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
   Navbar,
   NavbarToggler,
   NavbarBrand,
   Nav,
   NavItem,
   NavLink } from 'reactstrap';
-
-import { getCurrentUser, logOutCurrentUser } from "./lib/api.js";
-
 
 export default class MyNavbar extends Component {
   constructor(props) {
@@ -26,7 +16,6 @@ export default class MyNavbar extends Component {
     this.state = {
       isOpen: false
     };
-
   }
 
   toggle() {
@@ -38,54 +27,26 @@ export default class MyNavbar extends Component {
   render() {
     return (
       <div>
-        <Navbar color="faded" light>
+        <Navbar color="faded" light expand="md">
           <NavbarBrand href="/">Mission City Swing Registration</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav navbar>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Dance
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    <NavLink href="/dance">New Dance Form</NavLink>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <NavLink href="/dance-checkin">Dance Checkin</NavLink>
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Students
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    <NavLink href="/new-student">New Student Form</NavLink>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <NavLink href="/returning">Returning Student Checkin</NavLink>
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Admin
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    <NavLink href="/new-user">New Admin User</NavLink>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <NavLink href="/signin">Sign In Form</NavLink>
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    <NavLink href="#" onClick={logOutCurrentUser()}>Sign Out</NavLink>
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/new-user">New User Form</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/signin">Sign In Form</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/new-student">New Student Form</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/returning">Returning Student Checkin</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/dance">Dance Form</NavLink>
+              </NavItem>
             </Nav>
           </Collapse>
         </Navbar>
